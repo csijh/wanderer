e46c56ca57000ae7d4ccbb19130230f537417cea
csijh <csijh@bristol.ac.uk>
2017-12-19 13:54:22 +0000

    Update maze to use Game interface

--- java/maze/Item.java
+++ java/maze/Item.java
@@ -1,64 +1,65 @@
 package maze;
 import model.*;
 
-/* By Ian Holyer, 2017. Free and open source: see licence.txt.
+/* Maze entity class. Free and open source: see licence.txt.
 
-This class represents any item in the maze game. The different types of item are
-distinguished by a character code.
+This class represents any entity in the maze game. (It is called Item to avoid a
+name clash in the Makefile with wanderer.Entity)
 
-The Player item represents the user.  Space items are blank spaces where the
-player can go.  Wall items provide impenetrable barriers, and in particular
-surround a level. Star items are collected by the player to solve the level. */
+The Player entity represents the user.  Spaces are where the player can go.
+Walls are impenetrable barriers, in particular surrounding a level. Stars are
+collected by the player to solve the level. */
 
 class Item extends Cell<Item> {
-    private char type;
-
-    static String STARS = "STARS";
+    static String STARS = "STARS", PLAYER = "PLAYER";
     static final char Player = '@', Space = '.', Wall = '#', Star = '*';
-    static final Item[] samples = {
-        new Item(Player), new Item(Space), new Item(Wall), new Item(Star)
-    };
 
-    Item(char t) { type = t; }
-
-    // Override the required Cell methods.
-
-    public char code() { return type; }
-
-    public String image() {
-        switch (type) {
-            case Player: return "images/Player.png";
-            case Space: return "images/Space.png";
-            case Wall: return "images/Wall.png";
-            case Star: return "images/Star.png";
-            default: throw new Error("Unknown type");
-        }
-    }
-
-    public String status() {
-        int n = count(STARS);
-        if (n > 0) return "Stars: " + n;
-        else return "Success!";
-    }
-
-    public void hatch() {
-        if (type == Star) add(STARS, 1);
-        else if (type == Player) {
+    // Count stars, remember the player.
+    public void wake() {
+        if (type() == Star) add(STARS, 1);
+        else if (type() == Player) {
             set(PLAYER, this);
             agent(true);
         }
     }
 
     public void act() {
-        if (type != Player) return;
+        if (type() != Player) return;
         Direction go = Direction.go(command());
         Item it = find(go);
-        if (it.type != Space && it.type != Star) return;
+        if (it.type() != Space && it.type() != Star) return;
         swap(it);
-        if (it.type == Star) {
-            it.type = Space;
+        if (it.type() == Star) {
+            it.mutate(Space);
             subtract(STARS, 1);
         }
+    }
+
+    static Item create(char type) {
+        switch (type) {
+            case Player: case Space: case Wall: case Star: return new Item();
+            default: return null;
+        }
+    }
+
+    static String image(char type) {
+        switch (type) {
+            case Player: return "images/Player.png";
+            case Space: return "images/Space.png";
+            case Wall: return "images/Wall.png";
+            case Star: return "images/Star.png";
+            default: return null;
+        }
+    }
+
+    static Item player(Level<Item> level) {
+        return level.entity(PLAYER);
+    }
+
+    static String status(Level l) {
+        int n = l.count(STARS);
+        if (n > 0) return "Stars: " + n;
+        else return "Success!";
     }
 
     public static void main(String[] args) {
--- java/maze/Maze.java
+++ java/maze/Maze.java
@@ -4,26 +4,30 @@ import view.*;
 import javafx.application.*;
 import javafx.stage.*;
 
-/* By Ian Holyer, 2017. Free and open source: see licence.txt.
+/* Maze game. Free and open source: see licence.txt.
 
 This is a cut-down version of the wanderer game, to illustrate simple use of the
 wanderer framework. Collect all the stars to win the level. (Note the help pages
 are wanderer's.) */
 
-public class Maze extends Application {
+public class Maze extends Application implements Game<Item> {
     private Controller<Item> controller;
 
     public void start(Stage stage) {
-        Level<Item> level = new Level<>(Item.samples);
-        for (Item x : Item.samples) x.init(level);
-        String[] levels = { "/levels/maze.txt" };
-        controller = new Controller<Item>("Maze", stage, level, levels);
-        stage.setScene(controller);
-        controller.setup(getParameters().getRaw());
-        stage.show();
-        controller.replay();
+        controller = new Controller<>(stage, this, getParameters().getRaw());
     }
 
     // Tidy up when the application closes (quit button or window close).
     public void stop() { controller.close(); }
+
+    // Implement the Game interface.
+    public String name() { return "Maze"; }
+    public int levelCount() { return 1; }
+    public String levelPath(int i) { return "/levels/maze.txt"; }
+    public Item hatch(char type) { return new Item(); }
+    public String imagePath(char type) { return Item.image(type); }
+    public Cell<Item> player(Level<Item> level) { return Item.player(level); }
+    public String status(Level<Item> level) { return Item.status(level); }
+    public int score(Level<Item> level) { return level.count(Item.STARS); }
+    public boolean success(Level<Item> level) { return score(level) == 0; }
 }
